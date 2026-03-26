47ae3d24d1d7a63c38a96081432bcc46c4107c82
Stealyoung <bden97@gmail.com>
2026-03-26 23:32:41 +0500

    Управление автотестами с Pytest. Параметризация Pytest

---
+++ tests/test_authorization.py
@@ -0,0 +1,38 @@
+import pytest
+from playwright.sync_api import Playwright, Page, expect
+
+@pytest.fixture()
+def chromium_page(playwright: Playwright) -> Page:
+    # Открываем браузер и создаем новую страницу
+    browser=playwright.chromium.launch(headless=False)
+    yield browser.new_page()
+    browser.close() # Закрываем страницу после выполнения
+
+@pytest.mark.authorization
+@pytest.mark.regression
+@pytest.mark.parametrize("email, password", [
+    {"user.name@gmail.com", "password"},
+    {"user.name@gmail.com", "  "},
+    { "  ", "password"},
+])
+def test_wrong_email_or_password_authorization(chromium_page: Page, email: str, password: str):
+
+    # Переходим на страницу входа
+    chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+
+    # Заполняем поле email
+    email_input = chromium_page.get_by_test_id('login-form-email-input').locator('input')
+    email_input.fill(email)
+
+    # Заполняем поле пароль
+    password_input = chromium_page.get_by_test_id('login-form-password-input').locator('input')
+    password_input.fill(password)
+
+    # Нажимаем на кнопку Login
+    login_button = chromium_page.get_by_test_id('login-page-login-button')
+    login_button.click()
+
+    # Проверяем, что появилось сообщение об ошибке
+    wrong_email_or_password_alert = chromium_page.get_by_test_id('login-page-wrong-email-or-password-alert')
+    expect(wrong_email_or_password_alert).to_be_visible()
+    expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
