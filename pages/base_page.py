49304dc6f950123e87c01d7b507259de873e6483
Stealyoung <bden97@gmail.com>
2026-03-28 15:35:42 +0500

    Паттерны автоматизации тестирования UI. Знакомство с PageObject

---
+++ pages/base_page.py
@@ -0,0 +1,16 @@
+from playwright.sync_api import Page  # Импортируем класс Page
+
+class BasePage:
+    # Конструктор класса, принимающий объект Page
+    def __init__(self, page: Page):
+        self.page = page  # Присваиваем объект page атрибуту класса
+
+    # Метод для перехода на страницу
+    def visit(self, url: str):
+        self.page.goto(url)
+        # self.page.wait_for_timeout(10000)
+        # Закомментирована строка, тк мешает выполнению теста на личной машине
+
+    # Метод для перезагрузки страницы
+    def reload(self):
+        self.page.reload(wait_until='domcontentloaded')
