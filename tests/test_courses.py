e5e9dda0c6e1a3aee0fa8b3b41fa53128e867990
Stealyoung <bden97@gmail.com>
2026-03-30 21:55:26 +0500

    fix. Restore previous test from file test courses

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,12 +1,14 @@
 import pytest
+from playwright.sync_api import Page, expect
 
 from pages.courses_list_page import CoursesListPage, CheckVisibleCourseCardParams
 from pages.create_course_page import CreateCoursePage
 
+
 @pytest.mark.regression
 @pytest.mark.courses
 def test_create_course(
-    create_course_page: CreateCoursePage, courses_list_page: CoursesListPage
+        create_course_page: CreateCoursePage, courses_list_page: CoursesListPage
 ):
     create_course_page.visit(
         "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create"
@@ -40,4 +42,42 @@ def test_create_course(
             min_score="10",
             estimated_time="2 weeks",
         )
+    )
+
+@pytest.mark.courses
+@pytest.mark.regression
+def test_empty_courses_list(chromium_page_with_state: Page):  # Создаем тестовую функцию
+
+    # Переходим на страницу курсов
+    chromium_page_with_state.goto(
+        "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses"
+    )
+
+    # Проверяем, что есть заголовок Courses
+    courses_title = chromium_page_with_state.get_by_test_id(
+        "courses-list-toolbar-title-text"
+    )
+    expect(courses_title).to_be_visible()
+    expect(courses_title).to_have_text("Courses")
+
+    # Проверяем, что есть иконка пустого блока
+    empty_block_icon = chromium_page_with_state.get_by_test_id(
+        "courses-list-empty-view-icon"
+    )
+    expect(empty_block_icon).to_be_visible()
+
+    # Проверяем, что появился текст об отсутствии результатов
+    no_results_block = chromium_page_with_state.get_by_test_id(
+        "courses-list-empty-view-title-text"
+    )
+    expect(no_results_block).to_be_visible()
+    expect(no_results_block).to_have_text("There is no results")
+
+    # Проверяем, что появился текстовый блок под заголовком
+    text_description_block = chromium_page_with_state.get_by_test_id(
+        "courses-list-empty-view-description-text"
+    )
+    expect(text_description_block).to_be_visible()
+    expect(text_description_block).to_have_text(
+        "Results from the load test pipeline will be displayed here"
     )
