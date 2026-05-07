6ac7d013a130e751683b414a7553f81f31d09e8c
Stealyoung <bden97@gmail.com>
2026-05-08 00:51:08 +0500

    Паттерны автоматизации тестирования UI. Наращивание тестовой базы и рефакторинг

--- tests/Courses/test_courses.py
+++ tests/Courses/test_courses.py
@@ -59,3 +59,44 @@ class TestCourses:
             min_score="10",
             estimated_time="2 weeks",
         )
+
+    def test_edit_course(
+        self, create_course_page: CreateCoursePage, courses_list_page: CoursesListPage
+    ):
+        create_course_page.visit(
+            "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create"
+        )
+        create_course_page.create_course_form.fill(
+            title="Playwright",
+            estimated_time="2 weeks",
+            description="Playwright",
+            max_score="100",
+            min_score="10",
+        )
+        create_course_page.image_upload_widget.upload_preview_image(
+            file="./testdata/files/image.png"
+        )
+        create_course_page.create_course_toolbar_view.click_create_course_button()
+        courses_list_page.course_view.check_visible(
+            index=0,
+            title="Playwright",
+            max_score="100",
+            min_score="10",
+            estimated_time="2 weeks",
+        )
+        courses_list_page.course_view.menu.click_edit(0)
+        create_course_page.create_course_form.fill(
+            title="Automatization QA",
+            estimated_time="3 months",
+            description="Automatization QA",
+            max_score="200",
+            min_score="1",
+        )
+        create_course_page.create_course_toolbar_view.click_create_course_button()
+        courses_list_page.course_view.check_visible(
+            index=0,
+            title="Automatization QA",
+            estimated_time="3 months",
+            max_score="200",
+            min_score="1",
+        )
