e7aae7786f759a19da32a82327616e4bbf8a1164
Stealyoung <bden97@gmail.com>
2026-05-14 23:45:13 +0500

    Edit file from lesson 8.4

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,51 +1,49 @@
+import allure
 import pytest
+from _pytest.fixtures import SubRequest
 from playwright.sync_api import Playwright, Page
+
+from pages.Authentication.registration_page import RegistrationPage
 
 
 @pytest.fixture
-def chromium_page(playwright: Playwright) -> Page:
-    browser = playwright.chromium.launch(headless=False)
-    yield browser.new_page()
-    browser.close()
+def chromium_page(request: SubRequest, playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)  # Запускаем браузер
+    context = browser.new_context()
+    context.tracing.start(screenshots=True, snapshots=True, sources=True)
+
+    yield context.new_page()  # Открываем новую страницу и передаем ее в тест
+    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
+    browser.close()  # После выполнения теста закрываем браузер
+
+    allure.attach.file(source=f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
 
 
 @pytest.fixture(scope="session")
-def initialize_browser_state(
-    playwright: Playwright,
-):  # Создаем функцию заполнения формы регистрации
-    # Открываем браузер и создаем новую страницу
+def initialize_browser_state(playwright: Playwright):
     browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()  # Создание контекста
-    page = context.new_page()  # Создание страницы
-    # Переходим на страницу регистрации
-    page.goto(
-        "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration"
+    context = browser.new_context()
+    page = context.new_page()
+
+    registration_page = RegistrationPage(page=page)
+    registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    registration_page.registration_form.fill_registration_form(
+        email='user.name@gmail.com', username='username', password='password'
     )
-    # Заполняем поле email
-    email_input = page.get_by_test_id("registration-form-email-input").locator("input")
-    email_input.fill("user.name@gmail.com")
-    # Заполняем поле username
-    username_input = page.get_by_test_id("registration-form-username-input").locator(
-        "input"
-    )
-    username_input.fill("username")
-    # Заполняем поле password
-    password_input = page.get_by_test_id("registration-form-password-input").locator(
-        "input"
-    )
-    password_input.fill("password")
-    # Нажимаем на кнопку Registration
-    registration_button = page.get_by_test_id("registration-page-registration-button")
-    registration_button.click()
-    # Сохраняем состояние браузера (куки и localStorage) в файл для дальнейшего использования
+    registration_page.click_registration_button()
+
     context.storage_state(path="browser-state.json")
+    browser.close()
 
 
 @pytest.fixture
-def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(
-        storage_state="browser-state.json"
-    )  # Указываем файл с сохраненным состоянием
-    yield context.new_page()  # Создаем контекст для будущей открытой страницы
-    browser.close()
+def chromium_page_with_state(request: SubRequest, initialize_browser_state, playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)  # Запускаем браузер
+    context = browser.new_context(storage_state="browser-state.json")  # Создаем контекст вместе с сохраненным состоянием бразуера
+    context.tracing.start(screenshots=True, snapshots=True, sources=True)
+
+    yield context.new_page()  # Открываем новую страницу и передаем ее в тест
+    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
+    browser.close()  # После выполнения теста закрываем браузер
+
+    allure.attach.file(source=f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
